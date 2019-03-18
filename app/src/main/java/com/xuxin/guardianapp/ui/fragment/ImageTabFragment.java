6b4a16bdce2b464e4e0d52dab709b46ba0561e12
zhousaito <zhousaito@163.com>
2019-03-18 15:04:52 +0800

    考核页面跳转

--- app/src/main/java/com/xuxin/guardianapp/ui/fragment/ImageTabFragment.java
+++ app/src/main/java/com/xuxin/guardianapp/ui/fragment/ImageTabFragment.java
@@ -1,16 +1,20 @@
 package com.xuxin.guardianapp.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.xuxin.guardianapp.R;
 import com.xuxin.guardianapp.base.BaseFragment;
+import com.xuxin.guardianapp.ui.activity.ExamineActivity;
 
 import butterknife.BindView;
 
 public class ImageTabFragment extends BaseFragment {
     @BindView(R.id.iv_tab)
     ImageView ivTab;
+    private int mImg;
 
     public static ImageTabFragment newInstance(int drawableId) {
 
@@ -29,7 +33,23 @@ public class ImageTabFragment extends BaseFragment {
     @Override
     protected void initView() {
         super.initView();
-        int img = getArguments().getInt("img");
-        ivTab.setImageResource(img);
+        mImg = getArguments().getInt("img");
+        ivTab.setImageResource(mImg);
+
+        ivTab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                skipActivity();
+            }
+        });
+
+    }
+
+    private void skipActivity() {
+        switch (mImg) {
+            case R.drawable.exam:
+                getContext().startActivity(new Intent(getContext(), ExamineActivity.class));
+                break;
+        }
     }
 }
