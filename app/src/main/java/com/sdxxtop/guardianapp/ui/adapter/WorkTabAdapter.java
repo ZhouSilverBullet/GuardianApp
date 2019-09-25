e6aeeb38997b875bdd1a90fd4bf73d007b8f3da2
liwenbo <18614005205@163.com>
2019-09-25 09:29:54 +0800

    关闭鹰眼定位省电提醒,更新版本1.1.6

--- app/src/main/java/com/sdxxtop/guardianapp/ui/adapter/WorkTabAdapter.java
+++ app/src/main/java/com/sdxxtop/guardianapp/ui/adapter/WorkTabAdapter.java
@@ -30,8 +30,8 @@ import com.sdxxtop.guardianapp.ui.activity.SectionEventActivity;
 public class WorkTabAdapter extends BaseAdapter {
 
     private int[] imgRes = {R.drawable.icon_shijian_work, R.drawable.icon_shuju_work, R.drawable.icon_qiye_work,
-            R.drawable.icon_bumen_work, R.drawable.icon_shangbao_work, R.drawable.icon_xuncha_work, R.drawable.icon_bumen_work, R.drawable.icon_bumen_work};
-    private String[] titleRes = {"事件统计", "工作数据", "企业数据", "部门事件", "我的上报", "我的巡查", "网格事件", "工作轨迹"};
+            R.drawable.icon_bumen_work, R.drawable.icon_shangbao_work, R.drawable.icon_xuncha_work, R.drawable.icon_bumen_work};
+    private String[] titleRes = {"事件统计", "工作数据", "企业数据", "部门事件", "我的上报", "我的巡查", "网格事件"};
 
     @Override
     public int getCount() {
