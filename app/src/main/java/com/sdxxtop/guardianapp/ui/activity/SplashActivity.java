65787c3833645b90d0eae0656007c7a80798d33e
zhousaito <zhousaito@163.com>
2019-12-12 15:43:19 +0800

    安装完成后打开app，然后home键退出后，在从桌面点击，就会出现重新启动应用的现象

--- app/src/main/java/com/sdxxtop/guardianapp/ui/activity/SplashActivity.java
+++ app/src/main/java/com/sdxxtop/guardianapp/ui/activity/SplashActivity.java
@@ -28,7 +28,10 @@ public class SplashActivity extends BaseMvpActivity<SplashPresenter> implements 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        if((getIntent().getFlags() & Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT) != 0){
+            finish();
+            return;
+        }
         if (!this.isTaskRoot()) {
             Intent intent = getIntent();
             String action = intent.getAction();
